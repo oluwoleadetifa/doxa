f47d95751596607e0db341249612c9558f95e2c9
oluwoleadetifa <66252275+oluwoleadetifa@users.noreply.github.com>
2022-12-26 20:49:35 -0500

    commit 1

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import '../styles/style.css';
+import Navbar from './layout/Navbar';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home-body'>
+      <Navbar />
+      <div className='home-content'>
+        <div className='left'>
+          <div>Become a coding guru</div>
+          <h2>Begin your coding journey</h2>
+          <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error eum ipsum perferendis modi impedit eveniet ut fugit rem, ipsam voluptas rerum temporibus quisquam provident praesentium, ullam laboriosam nam, aliquid ea?</div>
+        </div>
+        <div className='right'>
+          <img className='image-wrapper' src='../assets/images/sea.jpeg' alt=''/>
+        </div>
+      </div>
+    </div>
   )
 }
 
